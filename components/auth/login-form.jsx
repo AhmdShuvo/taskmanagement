12d2cbc16398356a94a2f331bc6aa9e1bb40c509
Tanjir Ahmed <74643721+AhmdShuvo@users.noreply.github.com>
2025-03-22 13:43:04 +0600

    update login page

--- components/auth/login-form.jsx
+++ components/auth/login-form.jsx
@@ -97,10 +97,10 @@ const LogInForm = () => {
         <SiteLogo className="h-10 w-10 2xl:w-14 2xl:h-14 text-primary" />
       </Link>
       <div className="2xl:mt-8 mt-6 2xl:text-3xl text-2xl font-bold text-default-900">
-        Hey, Hello 👋
+         👋
       </div>
       <div className="2xl:text-lg text-base text-default-600 2xl:mt-2 leading-6">
-        Enter the information you entered while registering.
+        Enter the information to login.
       </div>
       <form onSubmit={handleSubmit(onSubmit)} className="mt-5 2xl:mt-7">
         <div>
